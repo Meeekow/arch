dad33ba28014a31495220f838e63638fbd6e1956
Meeko <mynameismeeko@gmail.com>
2022-03-20 02:57:14 +0800

    refactored

--- bash-scripts/xmonad-stack.sh
+++ bash-scripts/xmonad-stack.sh
@@ -3,34 +3,37 @@
 # install dependencies first
 sudo pacman -S --needed git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf
 
-# make directory for stack xmonad
-mkdir -p ~/.config/xmonad && cd ~/.config/xmonad
-
-# copy xmonad.hs config to ~/.config/xmonad
-cp ~/arch/dotfiles/xmonad.hs .
+# go to ~/.config/xmonad directory
+cd ~/.config/xmonad/
 
 # clone github repo of xmonad, xmonad-contrib and xmobar
 git clone https://github.com/xmonad/xmonad
 git clone https://github.com/xmonad/xmonad-contrib
+# git clone https://github.com/jaor/xmobar
 
 # install stack
+cd ~/.config/xmonad/
 sudo pacman -S --needed stack
 
+# make sure stack is up to date
+cd ~/.config/xmonad/
+stack upgrade
+
 # initialize stack in ~/.config/xmonad
+cd ~/.config/xmonad/
 stack init
 
 # add path to path env variables
 export PATH="${PATH}:$HOME/.local/bin"
 
 # install xmonad
+cd ~/.config/xmonad/
 stack install
 
 # copy custom build script for xmonad-stack
-sudo cp /arch/bash-scripts/build .
+cd ~/.config/xmonad/
+cp /arch/bash-scripts/build ~/.config/xmonad/
 chmod +x build
-
-# make entry for lightdm display manager
-sudo cp /arch/dotfiles/xmonad.desktop /usr/share/xsessions/
 
 # symlink for recompile
 sudo ln -s ~/.local/bin/xmonad /usr/bin
