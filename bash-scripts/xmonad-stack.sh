a78eb4fbf2e3b988b843f3f93c2a06c935096e53
Meeko <mynameismeeko@gmail.com>
2022-03-19 16:13:42 +0800

    initial commit

---
+++ bash-scripts/xmonad-stack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install dependencies first
+sudo pacman -S --needed git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf
+
+# make directory for stack xmonad
+mkdir -p ~/.config/xmonad && cd ~/.config/xmonad
+
+# copy xmonad.hs config to ~/.config/xmonad
+cp ~/arch/dotfiles/xmonad.hs .
+
+# clone github repo of xmonad, xmonad-contrib and xmobar
+git clone https://github.com/xmonad/xmonad
+git clone https://github.com/xmonad/xmonad-contrib
+
+# install stack
+sudo pacman -S --needed stack
+
+# initialize stack in ~/.config/xmonad
+stack init
+
+# add path to path env variables
+export PATH="${PATH}:$HOME/.local/bin"
+
+# install xmonad
+stack install
