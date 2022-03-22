2105c3d9ec55d4fabbfae5bf8f443ca2b6ee250d
Meeko <mynameismeeko@gmail.com>
2022-03-22 18:48:49 +0800

    refactored comments for more verbosity

--- bash-scripts/screen-timeout.sh
+++ bash-scripts/screen-timeout.sh
@@ -1,22 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 
-
-# Turn off screen timeout.
+# turn off screen timeout
 xset s off
 xset s 0 0
 xset -dpms
 
-# Turn off auto-repeat for key presses.
+# turn off auto-repeat for key press
 xset -r
 
-# Force 'Full Composition Pipeline'
+# force 'Full Composition Pipeline'
 # nvidia-settings -a CurrentMetaMode="DP-0:3440x1440_144 +0+0 {ForceFullCompositionPipeline=On}"
 
-# Disable 'allow flipping' (0=True, 1=False)
+# disable 'allow flipping' (0=True, 1=False)
 nvidia-settings -a 'AllowFlipping=0'
 
-# Set PowerMizer to prefer 'Maximum Performance'
+# set PowerMizer to prefer 'Maximum Performance'
 nvidia-settings -a "[gpu:0] / GpuPowerMizerMode = 1"
 
-# Set 'openGL' to high quality (use 3 to use high performance)
+# set 'openGL' to high quality (use 3 to use high performance)
 nvidia-settings -a "OpenGLImageSettings = 0"
