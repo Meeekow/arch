94dd287e7b5aadc0e71c5094263eca88440a8f25
Meeko <mynameismeeko@gmail.com>
2022-04-02 03:54:44 +0800

    experimental tweaks added

--- bash-scripts/screen-timeout.sh
+++ bash-scripts/screen-timeout.sh
@@ -8,6 +8,9 @@ xset -dpms
 # turn off auto-repeat for key press
 xset -r
 
+# Better Perf Tweaks?
+# https://forum.level1techs.com/t/nvidia-gpu-settings-guide-for-better-performance/131660/6
+
 # force 'Full Composition Pipeline'
 # nvidia-settings -a CurrentMetaMode="DP-0:3440x1440_144 +0+0 {ForceFullCompositionPipeline=On}"
 
@@ -15,7 +18,7 @@ xset -r
 nvidia-settings -a 'AllowFlipping=0'
 
 # set PowerMizer to prefer 'Maximum Performance'
-nvidia-settings -a "[gpu:0] / GpuPowerMizerMode = 1"
+# nvidia-settings -a "[gpu:0] / GpuPowerMizerMode = 1"
 
 # set 'openGL' to high quality (use 3 to use high performance)
 nvidia-settings -a "OpenGLImageSettings = 0"
