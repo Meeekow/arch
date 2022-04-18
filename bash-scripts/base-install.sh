fb481e364ff85c434c1d4595f6f75578f84906d5
Meeko <mynameismeeko@gmail.com>
2022-04-18 20:30:10 +0800

    fixed typo

--- bash-scripts/base-install.sh
+++ bash-scripts/base-install.sh
@@ -18,7 +18,7 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 # change keyboard layout accordingly
 # uncomment "Dvorak" line if using Standard Qwerty
 # defaults to OWERTY layout since I am using a programmable keyboard
-# echo "KEYMAP=dvorak" >> /etc/vconsole.conf
+#echo "KEYMAP=dvorak" >> /etc/vconsole.conf
 echo "KEYMAP=us" >> /etc/vconsole.conf
 
 # set hostname
@@ -37,7 +37,7 @@ echo root:mm | chpasswd
 pacman -S --noconfirm --needed dnsmasq network-manager-applet networkmanager openresolv r8168 base-devel linux-headers dialog dosfstools mtools blueman bluez bluez-utils alsa-utils pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth ntfs-3g gvfs lxsession pcmanfm mtpfs gvfs-mtp ffmpeg vlc firefox exfatprogs bash-completion dmenu kitty nitrogen reflector rsync scrot xclip
 
 # uncomment if grub bootloader will be used instead of systemd-boot
-# pacman -S --noconfirm --needed efibootmgr grub os-prober dnsmasq network-manager-applet networkmanager openresolv r8168 base-devel linux-headers dialog dosfstools mtools blueman bluez bluez-utils alsa-utils pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth ntfs-3g gvfs lxsession pcmanfm mtpfs gvfs-mtp ffmpeg vlc firefox bash-completion dmenu kitty nitrogen reflector rsync scrot xclip
+#pacman -S --noconfirm --needed efibootmgr grub os-prober dnsmasq network-manager-applet networkmanager openresolv r8168 base-devel linux-headers dialog dosfstools mtools blueman bluez bluez-utils alsa-utils pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth ntfs-3g gvfs lxsession pcmanfm mtpfs gvfs-mtp ffmpeg vlc firefox bash-completion dmenu kitty nitrogen reflector rsync scrot xclip
 
 # display server
 pacman -S --noconfirm --needed xorg
@@ -49,15 +49,15 @@ pacman -S --noconfirm --needed nvidia nvidia-utils nvidia-settings
 pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
 
 # uncomment the line below if xmonad/xmobar from pacman is preferred instead of the xmonad/xmobar via stack
-# pacman -S --noconfirm --needed xmonad xmonad-contrib xmobar
+#pacman -S --noconfirm --needed xmonad xmonad-contrib xmobar
 
 # uncomment if grub will be used instead of systemd-boot
 # install Grub bootloader
-# grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 # uncomment if grub will be used instead of systemd-boot
 # make a config for Grub
-# grub-mkconfig -o /boot/grub/grub.cfg
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 # systemd-boot basic config
 bootctl --path=/boot install
@@ -74,7 +74,7 @@ echo "options root=/dev/sda2 rw quiet splash" >> /boot/loader/entries/arch.conf
 modprobe r8168
 echo "blacklist r8169" >> /etc/modprobe.d/r8169_blacklist.conf
 # uncomment line below if using btrfs instead of ext4
-# sed -i -e '7s/()/(btrfs)/' /etc/mkinitcpio.conf
+#sed -i -e '7s/()/(btrfs)/' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 # enable packages to run on boot
@@ -99,7 +99,6 @@ cp /arch/resource/xmonad/xmonad.desktop /usr/share/xsessions/
 
 # remove repo from 'root' folder
 rm -rf /arch/
-echo "Github repository removed from root directory."
 
 
 printf "\e[1;32mDone! Type exit, umount -R /mnt and reboot.\e[0m\n"
