0547cd345735f99c468af4ff0edc932102f20966
Meeko <mynameismeeko@gmail.com>
2023-05-23 17:37:14 +0800

    removed this conf

--- bash-scripts/post-install.sh
+++ bash-scripts/post-install.sh
@@ -39,7 +39,7 @@ sudo sed -i -e '1s/^/server=1.1.1.1\n/' /etc/dnsmasq.conf
 sudo sed -i -e '2s/^/server=1.0.0.1\n/' /etc/dnsmasq.conf
 sudo sed -i -e '23s/.//' /etc/dnsmasq.conf
 sudo sed -i -e '60s/.//' /etc/dnsmasq.conf
-sudo sed -i -e '123s/#listen-address=/listen-address=::1.127.0.0.1/' /etc/dnsmasq.conf
+#sudo sed -i -e '123s/#listen-address=/listen-address=::1.127.0.0.1/' /etc/dnsmasq.conf
 sudo sed -i -e '577s/#cache-size=150/cache-size=1000/' /etc/dnsmasq.conf
 # uncomment the ff. 2 lines if ipv6 is needed
 #sudo sed -i -e '3s/^/server=2606:4700:4700::1111\n/' /etc/dnsmasq.conf
