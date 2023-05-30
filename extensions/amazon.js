6984a25d55d499fdff2e9b48f3e152095273ce90
Meeko <mynameismeeko@gmail.com>
2023-05-30 10:04:37 +0800

    used foreach instead of for loop

--- extensions/amazon.js
+++ extensions/amazon.js
@@ -1,27 +1,56 @@
-function search() {
+function detectOtherLang() {
+
+    let language = document.querySelectorAll('.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style span.a-size-base.a-color-secondary:nth-child(1)');
+
+    language.forEach( (e) => {
+        let pattern = e.innerHTML;
+        let re = new RegExp(pattern, "gi");
+        e.innerHTML = e.innerText.replace(re, match => `<mark>${match}</mark>`);
+    } );
+
+}
+
+
+function highlightExactMatch() {
 
     let searched = document.getElementById("twotabsearchtextbox").value.trim();
-
     searched = searched.replace(/hardcover |paperback |spiral /gi, "");
 
-    let listingTitle = document.querySelectorAll('.a-size-medium.a-color-base.a-text-normal');
-
+    let listingTitleMedium = document.querySelectorAll('.a-size-medium.a-color-base.a-text-normal');
     let listingTitleBasePlus = document.querySelectorAll('.a-size-base-plus.a-color-base.a-text-normal');
 
     let re = new RegExp(searched, "gi");
 
-    if ( listingTitle.length < listingTitleBasePlus ) {
-
-        for ( let i = 0; i < listingTitleBasePlus.length; ++i ) {
-            listingTitleBasePlus[i].innerHTML = listingTitleBasePlus[i].innerText.replace(re, match => `<mark>${match}</mark>`);
-        }
-
+    if ( listingTitleMedium.length < listingTitleBasePlus.length ) {
+        listingTitleBasePlus.forEach( (e) => {
+            e.innerHTML = e.innerText.replace(re, match => `<mark>${match}</mark>`);
+        } );
     } else {
+        listingTitleMedium.forEach( (e) => {
+            e.innerHTML = e.innerText.replace(re, match => `<mark>${match}</mark>`);
+        } );
+    }
 
-        for ( let i = 0; i < listingTitle.length; ++i ) {
-            listingTitle[i].innerHTML = listingTitle[i].innerText.replace(re, match => `<mark>${match}</mark>`);
-        }
+}
 
+
+function highlightPartialMatch() {
+
+    let searched = document.getElementById("twotabsearchtextbox").value.trim().split(" ");
+
+    let listingTitleMedium = document.querySelectorAll('.a-size-medium.a-color-base.a-text-normal');
+    let listingTitleBasePlus = document.querySelectorAll('.a-size-base-plus.a-color-base.a-text-normal');
+
+    let re = new RegExp(searched.join("|"), "gi");
+
+    if ( listingTitleMedium.length < listingTitleBasePlus.length ) {
+        listingTitleBasePlus.forEach( (e) => {
+            e.innerHTML = e.innerText.replace( re, match => `<mark>${match}</mark>` );
+        } );
+    } else {
+        listingTitleMedium.forEach( (e) => {
+            e.innerHTML = e.innerText.replace( re, match => `<mark>${match}</mark>` );
+        } );
     }
 
 }
@@ -29,15 +58,16 @@ function search() {
 
 document.getElementById("twotabsearchtextbox").addEventListener("input", () => {
 
-    search();
+    // highlightExactMatch();
+    highlightPartialMatch();
 
 });
 
 
+// dirty way to remove the blur when pressing escape while in input box/search box
 document.getElementById('nav-search-bar-form').addEventListener("keydown", e => {
 
     if ( e.keyCode === 27 ) {
-        // dirty way to remove the blur when pressing escape while in input box/search box
         // simulate a click to the amazon logo at the top left
         document.getElementById('nav-logo-sprites').click();
     }
@@ -45,21 +75,6 @@ document.getElementById('nav-search-bar-form').addEventListener("keydown", e => 
 });
 
 
-function detectOtherLang() {
-
-    let language = document.querySelectorAll('.a-section.a-spacing-none.puis-padding-right-small.s-title-instructions-style span.a-size-base.a-color-secondary:nth-child(1)');
-
-    for ( let i = 0; i < language.length; ++i ) {
-
-        let pattern = language[i].innerHTML;
-
-        let re = new RegExp(pattern, "gi");
-
-        language[i].innerHTML = language[i].innerText.replace(re, match => `<mark>${match}</mark>`);
-    }
-
-}
-
-
-setTimeout(search(), 5000);
+setTimeout(highlightPartialMatch(), 5000);
 setTimeout(detectOtherLang(), 5000);
+
