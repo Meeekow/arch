d5d9a269224e24aac66b7dee44f9d211964c5a47
Meeko <mynameismeeko@gmail.com>
2023-06-02 07:59:18 +0800

    open amazon link in new window, auto zoom once image loads

--- extensions/bookparse.js
+++ extensions/bookparse.js
@@ -1,35 +1,65 @@
 /*
+ * Wait for the image to load and zoom automatically
+**/
+const _waitForImg = setInterval( () => {
+    const img = document.querySelector("#task-specific-image");
+
+    if (img.complete) {
+        document.getElementById('task-specific-image').style.width = '1000px';
+        clearInterval(_waitForImg);
+    }
+
+}, 100);
+
+
+/*
  * Pressing escape while still in the title box
- * will escape insert mode and simulate click
- * on the generated amazon link
+ * will open the amazon generated link in a new window
 **/
 document.querySelector('input#link-title-input').addEventListener("keydown", e => {
     if ( e.keyCode === 27 ) {
-        document.querySelector('input#link-preview').click();
+        const _preview = document.getElementById('link-preview');
+        const _title_input = document.getElementById('link-title-input');
+        const _binding_selected = document.getElementById('link-binding-selected');
+
+        let title = _title_input.value;
+        let selected_b = _binding_selected.value;
+        let _binding = '';
+
+        switch(selected_b) {
+            case 'Paperback':
+                _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656022011';
+                break;
+            case 'Hardcover':
+                _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656020011';
+                break;
+            default:
+                _binding = '';
+                break;
+        }
+
+        title = title.replaceAll(' ', '+');
+        if ( title ) {
+        _preview.value = `https://www.amazon.com/s?k=${title}${_binding}`;
+        window.open(_preview.value, "_blank", "width=100,height=100");
+        }
     }
 });
 
 
 /*
- * Wait for 1 second and then fill out the details
- * base on the value of the binding box
+ * Wait for 100ms and set book binding
 **/
-function whatBinding() {
-    var binding = document.querySelector('#submission-container div.d-flex.mb-2:nth-child(2) input').value;
+const _whatBinding = setInterval( () => {
+    let binding = document.querySelector('#submission-container div.d-flex.mb-2:nth-child(2) input').value;
 
-    if (binding === "Hardcover") {
+    if ( binding === "Unknown" || binding === "Spiral" ) {
+        clearInterval(_whatBinding);
+    } else if ( binding === "Hardcover" ) {
         document.querySelector('#link-binding-selected').value = "Hardcover";
     } else if (binding === "Paperback") {
         document.querySelector('#link-binding-selected').value = "Paperback";
-    } else {
-        document.querySelector('#link-binding-selected').value = "Unknown";
     }
-};
 
-
-/*
- * Value is in milliseconds
- * Change the value accordingly
-**/
-setTimeout(whatBinding, 3000);
-
+    clearInterval(_whatBinding);
+}, 100)
