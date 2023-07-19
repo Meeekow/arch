bd92eed5d7287add4e38a719c78a212421b2c6b2
Meeko <mynameismeeko@gmail.com>
2023-07-20 06:20:47 +0800

    refactored

--- extensions/bookparse.js
+++ extensions/bookparse.js
@@ -24,8 +24,11 @@ function triggerEvent() {
 };
 
 
-function transcribe() {
+function transcribe(isPartialTextReplace = false) {
   const titleBox = document.querySelector(".form-nice-control.link-title-input");
+
+  let beforeWord = titleBox.value.slice(0, titleBox.selectionStart);
+  let afterWord = titleBox.value.slice(titleBox.selectionEnd);
 
   const speechRecognition = window.webkitSpeechRecognition;
   const recognition = new speechRecognition();
@@ -43,7 +46,14 @@ function transcribe() {
       .map((result) => result[0])
       .map((result) => result.transcript);
 
-    titleBox.value = transcript;
+    switch ( isPartialTextReplace ) {
+      case true:
+        replaceSelectedText(transcript, beforeWord, afterWord);
+        break;
+      default:
+        titleBox.value = transcript;
+    };
+
     triggerEvent();
   });
 
@@ -54,48 +64,15 @@ function transcribe() {
 };
 
 
-function partialTextReplace() {
-  const titleBox = document.querySelector(".form-nice-control.link-title-input");
-
-  let beforeWord = titleBox.value.slice(0, titleBox.selectionStart);
-  let afterWord = titleBox.value.slice(titleBox.selectionEnd);
-
-  const speechRecognition = window.webkitSpeechRecognition;
-  const recognition = new speechRecognition();
-  recognition.interimResults = true;
-
-  recognition.start();
-
-  recognition.addEventListener("audiostart", () => {
-      dictationMic.innerText = "Transcribing...";
-      dictationMic.style.cssText = "position: relative; left: 73%; background-color: #FAA0A0;";
-  });
-
-  recognition.addEventListener("result", (e) => {
-    const transcript = Array.from(e.results)
-      .map((result) => result[0])
-      .map((result) => result.transcript)
-      .join("");
-    replaceSelectedText(transcript, beforeWord, afterWord);
-  });
-
-  recognition.addEventListener("audioend", () => {
-      dictationMic.innerText = "Transcribe";
-      dictationMic.style.cssText = "position: relative; left: 73%; background-color: #F3F3F3;";
-  });
-};
-
-
 function replaceSelectedText(replacementText, beforeWord, afterWord) {
-  let titleBox = document.querySelector('.form-nice-control.link-title-input');
+  let titleBox = document.querySelector(".form-nice-control.link-title-input");
   let result = beforeWord + replacementText + ' ' + afterWord;
-  titleBox.value = result.replace(/\s+/g, " ");
-  triggerEvent();
+  titleBox.value = result.replace(/\s+/g, " ").replace(/\s+$/g, "");
 };
 
 
 function customActions() {
-  document.querySelector('.form-nice-control.link-title-input').addEventListener("keydown", (e) => {
+  document.querySelector(".form-nice-control.link-title-input").addEventListener("keydown", (e) => {
     if ( e.ctrlKey && e.key === 's' ) {
       e.preventDefault();
       transcribe();
@@ -103,34 +80,34 @@ function customActions() {
 
     if ( e.key === "Enter" ) {
       e.preventDefault();
-      partialTextReplace();
+      transcribe(true);
     };
 
     if ( e.key === "Escape" ) {
-      const _preview = document.querySelector('.form-nice-readonly-control.link-preview-value');
-      const _title_input = document.querySelector('.form-nice-control.link-title-input');
-      const _binding_selected = document.querySelector('.form-select.form-select-sm.link-binding-selected');
+      const _preview = document.querySelector(".form-nice-readonly-control.link-preview-value");
+      const _title_input = document.querySelector(".form-nice-control.link-title-input");
+      const _binding_selected = document.querySelector(".form-select.form-select-sm.link-binding-selected");
 
-      let title = _title_input.value.trim().toLowerCase();
+      let title = _title_input.value.toLowerCase().replace(/\s+/g, " ").replace(/\s+$/g, "");
       let selected_b = _binding_selected.value;
       let _binding = '';
 
-      switch(selected_b) {
-      case 'Paperback':
-        _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656022011';
-        break;
-      case 'Hardcover':
-        _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656020011';
-        break;
-      case 'Spiral':
-        _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A23488071011';
-        break;
-      case 'Board':
-        _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656019011';
-        break;
-      default:
-        _binding = '';
-        break;
+      switch (selected_b) {
+        case 'Paperback':
+          _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656022011';
+          break;
+        case 'Hardcover':
+          _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656020011';
+          break;
+        case 'Spiral':
+          _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A23488071011';
+          break;
+        case 'Board':
+          _binding = '&rh=n%3A283155%2Cp_n_feature_browse-bin%3A2656019011';
+          break;
+        default:
+          _binding = '';
+          break;
       };
 
       title = title.replaceAll(' ', '+');
@@ -147,18 +124,18 @@ function rotateImage() {
   document.addEventListener("keydown", (e) => {
     if ( e.target.nodeName != 'INPUT' ) {
       switch ( e.key ) {
-      case "ArrowLeft":
-        document.querySelector('.fulfillment-container').style.transform = 'rotate(270deg)';
-        break;
-      case "ArrowUp":
-        document.querySelector('.fulfillment-container').style.transform = 'rotate(0deg)';
-        break;
-      case "ArrowRight":
-        document.querySelector('.fulfillment-container').style.transform = 'rotate(90deg)';
-        break;
-      case "ArrowDown":
-        document.querySelector('.fulfillment-container').style.transform = 'rotate(180deg)';
-        break;
+        case "ArrowLeft":
+          document.querySelector('.fulfillment-container').style.transform = 'rotate(270deg)';
+          break;
+        case "ArrowUp":
+          document.querySelector('.fulfillment-container').style.transform = 'rotate(0deg)';
+          break;
+        case "ArrowRight":
+          document.querySelector('.fulfillment-container').style.transform = 'rotate(90deg)';
+          break;
+        case "ArrowDown":
+          document.querySelector('.fulfillment-container').style.transform = 'rotate(180deg)';
+          break;
       };
     };
   });
@@ -172,3 +149,4 @@ function main() {
 
 
 setInterval( () => { main() }, 1000);
+
