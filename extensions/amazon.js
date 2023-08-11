ba2defffd565d4e51546fab7da0c4bceaac9dbca
Meeko <mynameismeeko@gmail.com>
2023-08-12 05:00:57 +0800

    fix single quote regex/updated garbage data css selectors

--- extensions/amazon.js
+++ extensions/amazon.js
@@ -1,3 +1,73 @@
+function removeGarbageData() {
+  // customers who viewed items in your browsing history also viewed
+  waitForElement('#rhf-shoveler', () => { document.getElementById('rhf-shoveler').remove() });
+  waitForElement('.rhf-frame', () => { document.querySelector('.rhf-frame').remove() });
+
+  // footer
+  waitForElement('#navFooter', () => { document.getElementById('navFooter').remove() });
+
+  // sidebar
+  waitForElement('#s-refinements', () => { document.getElementById('s-refinements').remove() });
+
+  // 2nd, 3rd binding amazon details
+  document.querySelectorAll('.a-section.puis-price-hierarchy-twister-wrapper').forEach((e) => {
+    e.remove();
+  });
+
+  // price
+  document.querySelectorAll('.a-size-base.a-link-normal.s-no-hover.s-underline-text.s-underline-link-text.s-link-style.a-text-normal').forEach((e) => {
+    e.remove();
+  });
+
+  // delivery, ships to
+  document.querySelectorAll('.a-row.a-size-base.a-color-secondary.s-align-children-center').forEach((e) => {
+    e.remove();
+  });
+
+  // more buying choices
+  document.querySelectorAll('.a-section.a-spacing-none.a-spacing-top-mini .a-row.a-size-base.a-color-secondary').forEach((e) => {
+    e.remove();
+  });
+
+  // stock left
+  document.querySelectorAll('.a-size-base.a-color-price').forEach((e) => {
+    e.remove();
+  });
+
+  // synopsis, age
+  document.querySelectorAll('.a-section.a-spacing-none.a-spacing-top-micro.s-product-grid-adjustment').forEach((e) => {
+    e.remove();
+  });
+
+  // temporarily out of stock
+  document.querySelectorAll('span[aria-label="Temporarily out of stock."]').forEach((e) => {
+    e.remove();
+  });
+
+  // small business
+  document.querySelectorAll('.a-section.a-spacing-none.a-spacing-top-micro .s-align-children-center').forEach((e) => {
+    e.remove();
+  });
+}
+
+
+function turnIntoOneWord() {
+  document.querySelectorAll('.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal').forEach((e) => {
+    let title = [];
+    e.querySelectorAll('span.a-size-medium.a-color-base').forEach((el) => {
+      title.push(el.closest('span').textContent);
+      el.closest('span').remove();
+    });
+
+    const result = document.createElement('span');
+    result.classList = 'a-size-medium a-color-base a-text-normal';
+    result.innerHTML = title.join('').replaceAll(/’/g, "\'");
+    e.appendChild(result);
+    title.length = 0;
+  })
+}
+
+
 function changeBindingFontSize() {
   document.querySelectorAll('.a-row.a-size-base.a-color-base:nth-child(1) a.a-size-base.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-bold').forEach( e => {
     e.setAttribute('style', 'background: #FFC562 !important; font-size: 18px !important; color: #0F1111 !important');
@@ -32,40 +102,21 @@ function detectOtherLang() {
 }
 
 
-function removeGarbageData() {
-  // amazon ds quick view other bindings data
-  document.querySelectorAll('.a-row.a-row.a-spacing-mini').forEach((e) => {
-    e.remove();
-  });
+function highlightPartialMatch() {
+  let listingTitle = document.querySelectorAll('.a-size-medium.a-color-base.a-text-normal');
 
-  // prices
-  document.querySelectorAll('.sg-col-inner .a-row.a-size-base.a-color-base:nth-child(2)').forEach((e) => {
-    e.remove();
-  });
+  let patternToIgnore = ['a', 'i'];
 
-  // delivery, shipping
-  document.querySelectorAll('.a-row.a-size-base.a-color-secondary.s-align-children-center').forEach((e) => {
-    e.remove();
-  });
+  let searchQuery = document.getElementById('twotabsearchtextbox').value.trim().split(' ');
 
-  // delivery, shipping and other formats
-  document.querySelectorAll('.a-section.a-spacing-none.a-spacing-top-mini').forEach((e) => {
-    e.remove();
-  });
+  searchQuery = searchQuery.filter( (e) => { return patternToIgnore.indexOf(e) == -1 ? true : false } );
 
-  // stock left
-  document.querySelectorAll('.a-size-base.a-color-price').forEach((e) => {
-    e.remove();
-  });
+  searchQuery.sort((a, b) => { return b.length - a.length });
 
-  // get free / free with audible trial
-  document.querySelectorAll(".a-row.a-size-small.a-color-secondary").forEach((e) => {
-      e.remove();
-  })
+  let re = new RegExp(searchQuery.join('|'), 'gi');
 
-  // synopsis, ages
-  document.querySelectorAll('.sg-col-inner .a-section.a-spacing-none.a-spacing-top-micro.s-product-grid-adjustment').forEach((e) => {
-    e.remove();
+  listingTitle.forEach((e) => {
+    e.innerHTML = e.innerText.replace(re, (match) => `<mark>${match}</mark>`);
   });
 }
 
@@ -93,33 +144,7 @@ function highlightExactMatch() {
 }
 
 
-function highlightPartialMatch() {
-  let searched = document.getElementById("twotabsearchtextbox").value.trim().split(" ");
-
-  let pattern = ['a', 'i'];
-
-  searched = searched.filter( (e) => { return pattern.indexOf(e) == -1 ? true : false } );
-
-  let listingTitleMedium = document.querySelectorAll('.a-size-medium.a-color-base.a-text-normal');
-
-  let listingTitleBasePlus = document.querySelectorAll('.a-size-base-plus.a-color-base.a-text-normal');
-
-  let re = new RegExp(searched.join("|"), "gi");
-
-  if ( listingTitleMedium.length < listingTitleBasePlus.length ) {
-    listingTitleBasePlus.forEach( (e) => {
-      e.innerHTML = e.innerText.replace( re, match => `<mark>${match}</mark>` );
-    } );
-  } else {
-    listingTitleMedium.forEach( (e) => {
-      e.innerHTML = e.innerText.replace( re, match => `<mark>${match}</mark>` );
-    } );
-  }
-}
-
-
 document.getElementById("twotabsearchtextbox").addEventListener("input", () => {
-  // highlightExactMatch();
   highlightPartialMatch();
 });
 
@@ -133,21 +158,32 @@ document.getElementById('nav-search-bar-form').addEventListener("keydown", e => 
 });
 
 
+// https://makersaid.com/wait-for-element-to-exist-javascript/
+const waitForElement = (selector, callback) => {
+  const observer = new MutationObserver(function(mutations, me) {
+    const element = document.querySelector(selector);
+    if (element) {
+      callback(element);
+        // me.disconnect(); // Once the element has been found, we can stop observing for mutations
+        return;
+    }
+  });
+  observer.observe(document, { childList: true, subtree: true });
+}
+
+
 function main() {
   removeGarbageData();
+  turnIntoOneWord();
   changeBindingFontSize();
   detectWrongBinding();
   detectOtherLang();
   highlightPartialMatch();
 }
+main();
 
 
-setTimeout(main(), 5000); // 5 seconds
-
-
-// no idea what event for url change
-// naive implementation for now
-
+// no idea what event for url change, naive implementation for now
 // store current url
 let currentPage = location.href;
 
