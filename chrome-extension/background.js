ff79ced19e2399c6fdda174dfa2e5c74ce586444
Meeko <mynameismeeko@gmail.com>
2024-06-01 22:38:35 +0800

    move tab to a new window only if certain condition is met

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,6 +1,13 @@
-chrome.tabs.onCreated.addListener(async (tab) => {
-  const newTabId = await tab.id;
-  await chrome.windows.create({
-    tabId: newTabId
-  })
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  const _status = changeInfo.status;
+  const _url = changeInfo.url;
+
+  if (_url === undefined) { return }
+
+  if (_status === 'loading' &&
+      _url.includes('amazon.com')) {
+    chrome.windows.create({
+      tabId: tabId
+    })
+  }
 })
