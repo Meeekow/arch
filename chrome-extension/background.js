3868d4afcd1233d4279eab80b9f7a0444c45bbc7
Meeko <mynameismeeko@gmail.com>
2024-06-02 12:42:17 +0800

    faster implementation of moving a new tab to a new window

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,13 +1,13 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  const _status = changeInfo.status;
-  const _url = changeInfo.url;
-
-  if (_url === undefined) { return }
-
-  if (_status === 'loading' &&
-      _url.includes('amazon.com')) {
-    chrome.windows.create({
-      tabId: tabId
-    })
-  }
+chrome.tabs.onCreated.addListener(async function () {
+  const queryOptions = await { active: true, currentWindow: true };
+  await chrome.tabs.query(queryOptions, async function (tabs) {
+    const _id = await tabs[0].id;
+    const _currentUrl = await tabs[0].url;
+    const _pendingUrl = await tabs[0].pendingUrl;
+    if (_currentUrl === "" && _pendingUrl.includes('amazon.com')) {
+      await chrome.windows.create({
+        tabId: _id
+      })
+    }
+  })
 })
