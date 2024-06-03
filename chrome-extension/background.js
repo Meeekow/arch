b41a49e6c7e4416ec8f16b5fd2ffbf47ed7ed3c5
Meeko <mynameismeeko@gmail.com>
2024-06-03 14:20:04 +0800

    refactor

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,12 +1,10 @@
 chrome.tabs.onCreated.addListener(async function () {
   const queryOptions = await { active: true, currentWindow: true };
   await chrome.tabs.query(queryOptions, async function (tabs) {
-    const _id = await tabs[0].id;
-    const _currentUrl = await tabs[0].url;
-    const _pendingUrl = await tabs[0].pendingUrl;
-    if (_currentUrl === "" && _pendingUrl.includes('amazon.com')) {
+    const { id, url, pendingUrl } = await tabs[0];
+    if (url === "" && pendingUrl.includes('amazon.com')) {
       await chrome.windows.create({
-        tabId: _id
+        tabId: id
       })
     }
   })
