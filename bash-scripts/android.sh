84562d2478d790f1cc990a83914d274269cf5db8
Meeko <mynameismeeko@gmail.com>
2024-11-08 06:53:30 +0800

    updated path

--- bash-scripts/android.sh
+++ bash-scripts/android.sh
@@ -3,7 +3,7 @@
 
 # To use adb shell
 # https://stackoverflow.com/questions/2517493/adb-command-not-found-in-linux-environment
-sudo ln -s /android/platform-tools/adb /bin/adb
+sudo ln -s /path-to-directory/platform-tools/adb /bin/adb
 
 # To find/list all packages
 pm list packages -f | grep <package_name>
