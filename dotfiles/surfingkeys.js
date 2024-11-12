2e42d1c3020eea429736f58eccd305e123d62709
Meeko <mynameismeeko@gmail.com>
2024-11-13 03:42:50 +0800

    updated a hotkey

--- dotfiles/surfingkeys.js
+++ dotfiles/surfingkeys.js
@@ -173,7 +173,9 @@ mapkey('w', 'medium valuable', function() {
 
 // show hints for 'Use ASIN button'
 mapkey('r', 'show hints for Use ASIN button', function() {
-  Hints.create(".btn.btn-primary", Hints.dispatchMouseClick);
+  Hints.create(".btn.btn-primary", function(element) {
+    element.click();
+  });
 }, {domain: /bookparse.com\/dashboard\/.\/bookidentification.*/i} );
 
 // focus input box for book details like title, etc.
