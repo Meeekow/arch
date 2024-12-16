d3fd113443eeb8d5c023d6ed673036e055945090
Meeko <mynameismeeko@gmail.com>
2024-12-16 16:24:43 +0800

    initial commit

---
+++ resource/pihole/install.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+# Install Pi-hole
+curl -sSL https://install.pi-hole.net | bash
+
+# Change generated password by the installer
+pihole -a -p mm
+
+# Install Unbound
+sudo apt install unbound
+
+# Copy unbound.conf for pihole
+sudo cp $USER/arch/resource/pihole/pi-hole.conf /etc/unbound/unbound.conf.d/
+
+# Restart
+sudo service unbound restart
+
+# Test unbound (run this twice to see if it's working)
+dig pi-hole.net @127.0.0.1 -p 5335
+
+# Add this to settings in webui and then save
+# 127.0.0.1#5335
