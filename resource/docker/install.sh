680b77627cfa81b08c695402de5cf7383be775f2
Meeko <mynameismeeko@gmail.com>
2025-03-30 23:19:59 +0800

    install docker on debian 12

---
+++ resource/docker/install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# https://docs.docker.com/engine/install/ubuntu/
+
+# Uninstall old versions
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Prerequisites
+#sudo apt install gnome-terminal -y
+
+# Install using the apt repository
+# Add Docker's official GPG key:
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Make directory for jellyfin via docker
+#sudo mkdir -p /media/jellyfin/{cache,config,mnt}
+
+# Copy docker-compose.yml from personal repository
+#sudo cp $HOME/arch/resource/resource/docker/jellyfin/docker-compose.yml /media/jellyfin/
+
+# Add your user to the docker group
+sudo usermod -aG docker $USER
+
+# Add the jellyfin user to the render group
+sudo usermod -aG render $USER
+
